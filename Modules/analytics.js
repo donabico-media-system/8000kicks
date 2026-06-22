6471189a6ae700675d7299a8bb90be9b54d39baa
Donabico Global Media <donabico.media.system@gmail.com>
2026-06-23 00:53:31 +0700

    Delete analytics.js

--- Modules/analytics.js
+++
@@ -1,7 +0,0 @@
-// Modules/analytics.js
-console.log("Analytics đang theo dõi hành vi người dùng...");
-
-// Ghi nhận thời gian người dùng ở lại trang
-setTimeout(() => {
-    console.log("Người dùng đã ở trên trang 10 giây - Tỷ lệ chuyển đổi tiềm năng!");
-}, 10000);
