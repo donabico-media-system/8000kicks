dd86c3f19425c4dbc0aa8291b4aaa4d72bd3df8a
Donabico Global Media <donabico.media.system@gmail.com>
2026-06-22 20:14:10 +0700

    Update Drone-Matrix.js

--- .github/scripts/Drone-Matrix.js
+++ .github/scripts/Drone-Matrix.js
@@ -1,6 +1,6 @@
 /**
  * Drone-Matrix.js
- * EATHESEN V3000-Ω SOTA | Drone UX & Signal Harvesting Module
+ * EATHESEN V3000-Ω SOTA | Drone UX & Signal Harvesting Module + English Lock
  */
 
 class DroneMatrix {
